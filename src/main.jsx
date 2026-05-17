9f666e0cdba71dc04d452420a8bd23b7ea660e09
lalanedneed-prog <lalan.edneed@gmail.com>
2026-05-17 22:33:10 +0530

    updated project

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,18 @@ import { ThemeProvider, defaultTheme } from '@sparrowengg/twigs-react';
 import App from './App.jsx';
 import './styles/global.css';
 
+const customTheme = {
+  ...defaultTheme,
+  fonts: {
+    ...defaultTheme.fonts,
+    body: "'DM Sans', sans-serif",
+    heading: "'DM Sans', sans-serif",
+  },
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={customTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
